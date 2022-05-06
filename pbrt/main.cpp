2f66fb3711742cecf0e8a4ad78602196017547a0
Johan Solbakken <johan.hvaler@gmail.com>
2022-05-06 13:21:18 +0200

    Initial Commit

---
+++ pbrt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pbrt
+//
+//  Created by Johan Solbakken on 06/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
